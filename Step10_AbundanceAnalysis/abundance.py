8a503df881900482125139be092bfede944a1f53
Leon <leon.gasteiger@web.de>
2025-12-13 18:22:59 +0100

    Added last files for taxonomy and abundance

--- Step10_AbundanceAnalysis/abundance.py
+++ Step10_AbundanceAnalysis/abundance.py
@@ -14,7 +14,8 @@ TABLE = os.path.join("../", "Step6_Decontamination/", "filtered-table.qza")
 TAXONOMY = os.path.join("../", "Step9_TaxonomicAnalysis/", "taxonomy.qza")
 
 OUTOUT_ANCOMBC = "ancombc_results.qza"
-OUTPUT_ANCOMBC_VIZ = "ancombc_barplot.qzv"
+OUTPUT_ANCOMBC_DISEASE_VIZ = "ancombc_disease_barplot.qzv"
+OUTPUT_ANCOMBC_TOOTH_VIZ = "ancombc_tooth_barplot.qzv"
 
 # Running ANCOM-BC
 
@@ -49,8 +50,7 @@ except Exception as e:
 
 #saving results
 ancombc_subject.save(OUTOUT_ANCOMBC)
-print("ANCOM-BC completed successfully.")
-
+print("ANCOM-BC completed successfully without collapsing.")
 
 # Collapsing test at specific taxonomic level
 
@@ -67,6 +67,8 @@ except Exception as e:
     print(f"An error occurred while loading taxonomy artifact: {e}")
     exit(1)
 
+
+print("Collapsing table to Genus level and running ANCOM-BC with disease state...")
 try:
     table_genus, = taxa_actions.collapse(
     table=table,
@@ -77,10 +79,38 @@ try:
         table=table_genus,
         metadata=metadata,
         formula='disease_state',
+        reference_levels=['disease_state::healthy'],
     )
     l6_da_barplot_subject_viz, = composition_actions.da_barplot(
         data=l6_ancombc_subject,
-        significance_threshold=0.001,
+        significance_threshold=0.05,
+        
+    )
+except Exception as e:
+    print("Collapsing to Genus level failed:", e)
+    exit(1)
+
+#saving visualization
+l6_da_barplot_subject_viz.save(OUTPUT_ANCOMBC_DISEASE_VIZ)
+print("Collapsing to Genus level completed successfully.")
+
+
+
+print("Collapsing table to Genus level and running ANCOM-BC with tooth site...")
+try:
+    table_genus, = taxa_actions.collapse(
+    table=table,
+    taxonomy=taxonomy,
+    level=6,
+    )
+    l6_ancombc_subject, = composition_actions.ancombc(
+        table=table_genus,
+        metadata=metadata,
+        formula='Type',
+    )
+    l6_da_barplot_subject_viz, = composition_actions.da_barplot(
+        data=l6_ancombc_subject,
+        significance_threshold=0.05,
     )
 except Exception as e:
     print("Collapsing to Genus level failed:", e)
@@ -88,7 +118,7 @@ except Exception as e:
 
 
 #saving visualization
-l6_da_barplot_subject_viz.save(OUTPUT_ANCOMBC_VIZ)
+l6_da_barplot_subject_viz.save(OUTPUT_ANCOMBC_TOOTH_VIZ)
 print("Collapsing to Genus level completed successfully.")
 
 
